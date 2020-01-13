68bdb39d5004ae6ad04692f70bb6493817b1d1dc
Ely Lucas <ely@meta-tek.net>
2020-01-13 11:14:48 -0700

    Initial commit

---
+++ src/pages/Tab3.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+
+const Tab3Page: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Tab Three</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent></IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab3Page;
