997bf7d61bff91144496efa54958587552fc5ca0
jcesarmobile <jcesarmobile@gmail.com>
2023-10-13 19:37:53 +0200

    ignore dist

--- dist/assets/status-tap-de057600.js
+++
@@ -1,3 +0,0 @@
-import{e as a,j as i,k as c,w as d,s as l}from"./index-d3be6672.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
