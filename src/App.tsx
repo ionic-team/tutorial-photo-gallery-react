0be6ab7bb2884cedb0a3fcecdda7b95771e0bdf3
ShaneK <shane@shanessite.net>
2026-07-01 15:08:37 -0700

    fix(react): silence router future-flag and deprecated PWA meta console warnings

--- src/App.tsx
+++ src/App.tsx
@@ -38,7 +38,7 @@ setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
+    <IonReactRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <IonTabs>
         <IonRouterOutlet>
           <Route path="/tab1" element={<Tab1 />} />
